b3f7ddaf4a2e8fbd70a69b7dc843b277e4526628
kostadubovskiy <kostadubovskiy@gmail.com>
2022-05-27 13:24:20 -0400

    classes

--- v0/Penguin.java
+++ v0/Penguin.java
@@ -2,7 +2,7 @@ public class Penguin() {
   private int team; //0 = red; 1 = blue;
   private int weight; //0 = no momentum; 0 < --> weight * magnitude momentum
   public Penguin() {
-
+    //default peng
   }
 
   public int getTeam() {
---
+++ v0/Woo.java
@@ -0,0 +1,12 @@
+public class Woo() {
+
+  public Woo() {
+    //make default 2 teams of 5 players each
+  }
+
+  public Woo(int teamSize) {
+    //make 2 teams of teamSize players each
+  }
+
+
+}
