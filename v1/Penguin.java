a288a63cdd99621c0199e91cef4b7e9d4765918c
kostadubovskiy <kostadubovskiy@gmail.com>
2022-05-31 12:50:53 -0400

    v1 init

---
+++ v1/Penguin.java
@@ -0,0 +1,15 @@
+public class Penguin() {
+  private int team; //0 = red; 1 = blue;
+  private int weight; //0 = no momentum; 0 < --> weight * magnitude momentum
+  public Penguin() {
+    //default peng
+  }
+
+  public int getTeam() {
+    return team;
+  }
+
+  public int getTeam() {
+    return weight;
+  }
+}
