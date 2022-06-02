7ff8041bf66f996d2c1d6ba7dd556c0f25ce2c8d
kostadubovskiy <kostadubovskiy@gmail.com>
2022-06-02 12:37:36 -0400

    v3 init

---
+++ v3/Knockoff.java
@@ -0,0 +1,12 @@
+public class Knockoff {
+
+  public Knockoff() {
+    //make default 2 teams of 5 players each
+  }
+
+  public Knockoff(int players) {
+    //make 2 teams of teamSize players each
+  }
+
+
+}
---
+++ v3/Platoon.java
@@ -0,0 +1,12 @@
+public class Platoon {
+
+  public Platoon() {
+    //make default 2 teams of 5 players each
+  }
+
+  public Platoon(int color) {
+    //make 2 teams of teamSize players each
+  }
+
+
+}
